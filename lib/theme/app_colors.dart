d5fa802de60aa5f76df82e306208a36843123a5c
Han <275905127@qq.com>
2026-01-14 22:25:10 +0800

    Create app_colors.dart

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color lightBackground = Color(0xFFFFFFFF); 
+  static const Color lightCard = Color(0xFFF3F3F3);       
+  static const Color lightAlert = Color(0xFFE5E5E5);      
+  static const Color lightMenu = Color(0xFFEBEBEB);       
+  static const Color lightDivider = Color(0xFFFFFFFF);    
+
+  static const Color darkBackground = Color(0xFF000000);  
+  static const Color darkCard = Color(0xFF414141);        
+  static const Color darkAlert = Color(0xFF1B1B1B);       
+  static const Color darkMenu = Color(0xFF333333);        
+  static const Color darkDivider = Color(0xFF000000);     
+
+  static const Color brandYellow = Color(0xFFD2AE00);     
+}
