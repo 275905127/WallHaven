81d05cd866c50fbba8fbd1ce641a0beaea13acc5
Han <275905127@qq.com>
2026-01-14 23:31:29 +0800

    更新 foggy_app_bar.dart

--- lib/widgets/foggy_app_bar.dart
+++ lib/widgets/foggy_app_bar.dart
@@ -64,7 +64,8 @@ class FoggyAppBar extends StatelessWidget implements PreferredSizeWidget {
                   stops: const [0.0, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0], 
                 ),
               ),
-            )
+            ),
+          ),
           : null,
     );
   }
