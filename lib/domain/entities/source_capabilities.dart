77547b9e733b49ab07e47c5c6598e613489f7383
Han <275905127@qq.com>
2026-01-16 05:49:11 +0800

    更新 source_capabilities.dart

--- lib/domain/entities/source_capabilities.dart
+++ lib/domain/entities/source_capabilities.dart
@@ -23,31 +23,40 @@ class SourceCapabilities {
   final List<String> colorOptions;
 
   final bool supportsRating;
-  final List<RatingLevel> ratingOptions; // safe/questionable/explicit
+  final List<RatingLevel> ratingOptions;
 
   final bool supportsCategories;
-  final List<OptionItem> categoryOptions; // id/label
+  final List<OptionItem> categoryOptions;
 
   final bool supportsTimeRange;
-  final List<OptionItem> timeRangeOptions; // id/label
+  final List<OptionItem> timeRangeOptions;
 
   const SourceCapabilities({
     this.supportsText = true,
+
     this.supportsSort = false,
     this.sortByOptions = const [],
+
     this.supportsOrder = false,
+
     this.supportsResolutions = false,
     this.resolutionOptions = const [],
+
     this.supportsAtleast = false,
     this.atleastOptions = const [],
+
     this.supportsRatios = false,
     this.ratioOptions = const [],
+
     this.supportsColor = false,
     this.colorOptions = const [],
+
     this.supportsRating = false,
     this.ratingOptions = const [],
+
     this.supportsCategories = false,
     this.categoryOptions = const [],
+
     this.supportsTimeRange = false,
     this.timeRangeOptions = const [],
   });
