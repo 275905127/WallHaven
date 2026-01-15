41ae97c8ef65c04406305466c8ef70383b7a3b7d
Han <275905127@qq.com>
2026-01-16 06:54:09 +0800

    更新 wallpaper_repository.dart

--- lib/data/repository/wallpaper_repository.dart
+++ lib/data/repository/wallpaper_repository.dart
@@ -1,4 +1,6 @@
+import '../../domain/entities/filter_spec.dart';
 import '../../domain/entities/search_query.dart';
+import '../../domain/entities/source_kind.dart';
 import '../../domain/entities/wallpaper_detail_item.dart';
 import '../../domain/entities/wallpaper_item.dart';
 import '../sources/wallpaper_source.dart';
@@ -18,7 +20,13 @@ class WallpaperRepository {
     return _source.search(query);
   }
 
+  Future<WallpaperItem?> random(FilterSpec filters) {
+    return _source.random(filters);
+  }
+
   Future<WallpaperDetailItem?> detail(String id) {
     return _source.detail(id);
   }
+
+  SourceKind get kind => _source.kind;
 }
