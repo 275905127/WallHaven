ddd097108b6a983e13b14af5da2085aeb9472efe
Han <275905127@qq.com>
2026-01-16 04:46:41 +0800

    更新 wallpaper_source.dart

--- lib/data/sources/wallpaper_source.dart
+++ lib/data/sources/wallpaper_source.dart
@@ -1,10 +1,13 @@
 import '../../domain/entities/search_query.dart';
+import '../../domain/entities/source_capabilities.dart';
 import '../../domain/entities/wallpaper_detail_item.dart';
 import '../../domain/entities/wallpaper_item.dart';
 
 abstract class WallpaperSource {
-  String get sourceId; // SourceConfig.id
-  String get pluginId; // wallhaven / generic / ...
+  String get sourceId;
+  String get pluginId;
+
+  SourceCapabilities get capabilities;
 
   Future<List<WallpaperItem>> search(SearchQuery query);
 
