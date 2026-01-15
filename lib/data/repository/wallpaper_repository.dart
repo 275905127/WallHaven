9ac70c1cd72100f3383b6934627eeb5e8ea2794f
Han <275905127@qq.com>
2026-01-16 02:56:17 +0800

    更新 wallpaper_repository.dart

--- lib/data/repository/wallpaper_repository.dart
+++ lib/data/repository/wallpaper_repository.dart
@@ -1,4 +1,5 @@
 import '../../domain/entities/search_query.dart';
+import '../../domain/entities/wallpaper_detail_item.dart';
 import '../../domain/entities/wallpaper_item.dart';
 import '../sources/wallpaper_source.dart';
 
@@ -8,6 +9,7 @@ class WallpaperRepository {
   WallpaperRepository(this._source);
 
   String get sourceId => _source.sourceId;
+  String get pluginId => _source.pluginId;
 
   void setSource(WallpaperSource source) {
     _source = source;
@@ -15,5 +17,9 @@ class WallpaperRepository {
 
   Future<List<WallpaperItem>> search(SearchQuery query) {
     return _source.search(query);
+  }
+
+  Future<WallpaperDetailItem?> detail(String id) {
+    return _source.detail(id);
   }
 }
