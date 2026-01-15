55eae2c86a16dec33e538d07b6f5bc078422bafe
Han <275905127@qq.com>
2026-01-16 00:56:02 +0800

    更新 source_registry.dart

--- lib/sources/source_registry.dart
+++ lib/sources/source_registry.dart
@@ -1,5 +1,25 @@
 import 'source_plugin.dart';
 import 'wallhaven_source_plugin.dart';
+import 'source_plugin.dart';
+import 'wallhaven_source_plugin.dart';
+import 'simple_json_source_plugin.dart';
+
+class SourceRegistry {
+  final Map<String, SourcePlugin> _plugins;
+  SourceRegistry._(this._plugins);
+
+  factory SourceRegistry.defaultRegistry() {
+    return SourceRegistry._({
+      WallhavenSourcePlugin.kId: WallhavenSourcePlugin(),
+      SimpleJsonPlugin.kId: SimpleJsonPlugin(), // ✅ 新增
+    });
+  }
+
+  SourcePlugin plugin(String id) => _plugins[id]!;
+  SourcePlugin? tryPlugin(String id) => _plugins[id];
+
+  Iterable<SourcePlugin> get allPlugins => _plugins.values;
+}
 
 class SourceRegistry {
   final Map<String, SourcePlugin> _plugins;
