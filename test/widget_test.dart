05ade8550d01a3b810ac4f3c93591ac8d2ae59ea
Han <275905127@qq.com>
2026-01-17 02:56:55 +0800

    更新 widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:your_app/main.dart' as app; // TODO: 改成你的包名路径
+// TODO: 把 wallhaven 改成 pubspec.yaml 里的 name
+import 'package:wallhaven/main.dart';
 
 void main() {
   testWidgets('app boots', (WidgetTester tester) async {
-    // main.dart 里如果有 ensureInitialized / runApp，在测试里不要直接调用 main()
-    // 直接 pump root widget 最稳定。
-    await tester.pumpWidget(const app.AppRoot()); // TODO: 改成你的根 Widget 名字
-    await tester.pumpAndSettle(const Duration(milliseconds: 200));
+    // 如果你 main.dart 里有 AppRoot / MyApp，二选一
+    await tester.pumpWidget(const MyApp());
+    await tester.pumpAndSettle();
 
     expect(find.byType(MaterialApp), findsOneWidget);
   });
