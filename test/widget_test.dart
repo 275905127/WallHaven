7c5eb3c81b19ded2dee5403844d8291cbead06f6
Han <275905127@qq.com>
2026-01-16 11:18:25 +0800

    更新 widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,7 +4,7 @@ import 'package:wallhaven/main.dart';
 void main() {
   testWidgets('MyApp can build (smoke test)', (tester) async {
     // 只保证应用能启动并渲染一帧，不绑定任何具体页面/控件结构
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp());
     await tester.pump();
     expect(find.byType(MyApp), findsOneWidget);
   });
