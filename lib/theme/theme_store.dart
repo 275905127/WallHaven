2a02513db1dd2a2086190ef4706a9f30b43f5104
Han <275905127@qq.com>
2026-01-17 18:40:30 +0800

    更新 theme_store.dart

--- lib/theme/theme_store.dart
+++ lib/theme/theme_store.dart
@@ -11,28 +11,6 @@ import '../domain/entities/source_capabilities.dart';
 import '../sources/source_plugin.dart';
 import '../sources/source_registry.dart';
 
-// ----------------------------
-// ThemeScope
-// ----------------------------
-class ThemeScope extends InheritedWidget {
-  final ThemeStore store;
-
-  const ThemeScope({
-    super.key,
-    required this.store,
-    required super.child,
-  });
-
-  static ThemeStore of(BuildContext context) {
-    final scope = context.dependOnInheritedWidgetOfExactType<ThemeScope>();
-    assert(scope != null, 'ThemeScope not found in widget tree');
-    if (scope == null) throw FlutterError('ThemeScope not found in widget tree');
-    return scope.store;
-  }
-
-  @override
-  bool updateShouldNotify(ThemeScope oldWidget) => !identical(store, oldWidget.store);
-}
 
 // ----------------------------
 // ThemeStore
