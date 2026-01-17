1dcc994176ef3254e9ef16c21e6b93f171aa601f
Han <275905127@qq.com>
2026-01-17 21:45:01 +0800

    更新 app_intent.dart

--- lib/app/app_intent.dart
+++ lib/app/app_intent.dart
@@ -1,21 +1,14 @@
 // lib/app/app_intent.dart
-import 'package:flutter/material.dart';
-
 sealed class AppIntent {
   const AppIntent();
 }
 
-// -------- Navigation --------
 class OpenSettingsIntent extends AppIntent {
   const OpenSettingsIntent();
 }
 
 class OpenPersonalizationIntent extends AppIntent {
   const OpenPersonalizationIntent();
-}
-
-class PopRouteIntent extends AppIntent {
-  const PopRouteIntent();
 }
 
 class OpenDrawerIntent extends AppIntent {
@@ -26,44 +19,6 @@ class CloseDrawerIntent extends AppIntent {
   const CloseDrawerIntent();
 }
 
-// -------- Theme actions (write ThemeStore) --------
-class SetPreferredModeIntent extends AppIntent {
-  final ThemeMode mode;
-  const SetPreferredModeIntent(this.mode);
-}
-
-class SetEnableThemeModeIntent extends AppIntent {
-  final bool enabled;
-  const SetEnableThemeModeIntent(this.enabled);
-}
-
-class SetEnableCustomColorsIntent extends AppIntent {
-  final bool enabled;
-  const SetEnableCustomColorsIntent(this.enabled);
-}
-
-class SetAccentIntent extends AppIntent {
-  final Color color;
-  final String name;
-  const SetAccentIntent(this.color, this.name);
-}
-
-class SetCardRadiusIntent extends AppIntent {
-  final double value;
-  const SetCardRadiusIntent(this.value);
-}
-
-class SetImageRadiusIntent extends AppIntent {
-  final double value;
-  const SetImageRadiusIntent(this.value);
-}
-
-class SetCustomBackgroundColorIntent extends AppIntent {
-  final Color? color;
-  const SetCustomBackgroundColorIntent(this.color);
-}
-
-class SetCustomCardColorIntent extends AppIntent {
-  final Color? color;
-  const SetCustomCardColorIntent(this.color);
+class PopRouteIntent extends AppIntent {
+  const PopRouteIntent();
 }
