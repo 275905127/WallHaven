bfe0044df7955f3792ea94cfb54654b0391e3f86
Han <275905127@qq.com>
2026-01-17 10:48:04 +0800

    更新 settings_page.dart

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,18 +1,32 @@
-import 'package:flutter/material.dart';
-
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(title: const Text('设置')),
-      body: Center(
-        child: Text(
-          'SettingsPage',
-          style: TextStyle(color: theme.textTheme.bodyLarge?.color),
-        ),
+      body: ListView(
+        children: const [
+          ListTile(
+            leading: Icon(Icons.palette_outlined),
+            title: Text('主题'),
+            subtitle: Text('浅色 / 深色'),
+          ),
+          Divider(),
+
+          ListTile(
+            leading: Icon(Icons.filter_alt_outlined),
+            title: Text('默认筛选'),
+            subtitle: Text('分辨率 / 分类'),
+          ),
+          Divider(),
+
+          ListTile(
+            leading: Icon(Icons.info_outline),
+            title: Text('关于'),
+            subtitle: Text('版本信息'),
+          ),
+        ],
       ),
     );
   }
