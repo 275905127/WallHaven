9603efe63e03ed20d8a57d6caa08e83f8a6d5ba1
Han <275905127@qq.com>
2026-01-17 21:46:04 +0800

    更新 app_controller.dart

--- lib/app/app_controller.dart
+++ lib/app/app_controller.dart
@@ -8,70 +8,37 @@ import 'app_intent.dart';
 class AppController {
   final ThemeStore store;
 
-  final _effects = StreamController<AppEffect>.broadcast();
-  Stream<AppEffect> get effects => _effects.stream;
+  final StreamController<AppEffect> _eff = StreamController<AppEffect>.broadcast();
+
+  Stream<AppEffect> get effects => _eff.stream;
 
   AppController({required this.store});
 
   void dispatch(AppIntent intent) {
     switch (intent) {
-      // -------- Navigation --------
       case OpenSettingsIntent():
-        _effects.add(const NavigateToSettingsEffect());
+        _eff.add(const NavigateToSettingsEffect());
         return;
 
       case OpenPersonalizationIntent():
-        _effects.add(const NavigateToPersonalizationEffect());
-        return;
-
-      case PopRouteIntent():
-        _effects.add(const PopRouteEffect());
+        _eff.add(const NavigateToPersonalizationEffect());
         return;
 
       case OpenDrawerIntent():
-        _effects.add(const OpenDrawerEffect());
+        _eff.add(const OpenDrawerEffect());
         return;
 
       case CloseDrawerIntent():
-        _effects.add(const CloseDrawerEffect());
+        _eff.add(const CloseDrawerEffect());
         return;
 
-      // -------- Theme actions (write store) --------
-      case SetPreferredModeIntent(mode: final m):
-        store.setPreferredMode(m);
-        return;
-
-      case SetEnableThemeModeIntent(enabled: final v):
-        store.setEnableThemeMode(v);
-        return;
-
-      case SetEnableCustomColorsIntent(enabled: final v):
-        store.setEnableCustomColors(v);
-        return;
-
-      case SetAccentIntent(color: final c, name: final n):
-        store.setAccent(c, n);
-        return;
-
-      case SetCardRadiusIntent(value: final v):
-        store.setCardRadius(v);
-        return;
-
-      case SetImageRadiusIntent(value: final v):
-        store.setImageRadius(v);
-        return;
-
-      case SetCustomBackgroundColorIntent(color: final c):
-        store.setCustomBackgroundColor(c);
-        return;
-
-      case SetCustomCardColorIntent(color: final c):
-        store.setCustomCardColor(c);
+      case PopRouteIntent():
+        _eff.add(const PopRouteEffect());
         return;
     }
   }
 
   void dispose() {
-    _effects.close();
+    _eff.close();
   }
 }
