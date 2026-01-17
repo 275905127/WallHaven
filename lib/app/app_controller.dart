5e8d268517ec3f95c84dd64db326f73a941bca7c
Han <275905127@qq.com>
2026-01-17 21:03:52 +0800

    更新 app_controller.dart

--- lib/app/app_controller.dart
+++ lib/app/app_controller.dart
@@ -1,27 +1,73 @@
 // lib/app/app_controller.dart
 import 'dart:async';
 
+import '../theme/theme_store.dart';
 import 'app_effect.dart';
 import 'app_intent.dart';
 
 class AppController {
-  final _effects = StreamController<AppEffect>.broadcast();
+  final ThemeStore store;
 
+  final _effects = StreamController<AppEffect>.broadcast();
   Stream<AppEffect> get effects => _effects.stream;
+
+  AppController({required this.store});
 
   void dispatch(AppIntent intent) {
     switch (intent) {
+      // -------- Navigation --------
       case OpenSettingsIntent():
         _effects.add(const NavigateToSettingsEffect());
-        break;
+        return;
+
+      case OpenPersonalizationIntent():
+        _effects.add(const NavigateToPersonalizationEffect());
+        return;
+
+      case PopRouteIntent():
+        _effects.add(const PopRouteEffect());
+        return;
 
       case OpenDrawerIntent():
         _effects.add(const OpenDrawerEffect());
-        break;
+        return;
 
       case CloseDrawerIntent():
         _effects.add(const CloseDrawerEffect());
-        break;
+        return;
+
+      // -------- Theme actions (write store) --------
+      case SetPreferredModeIntent(mode: final m):
+        store.setPreferredMode(m);
+        return;
+
+      case SetEnableThemeModeIntent(enabled: final v):
+        store.setEnableThemeMode(v);
+        return;
+
+      case SetEnableCustomColorsIntent(enabled: final v):
+        store.setEnableCustomColors(v);
+        return;
+
+      case SetAccentIntent(color: final c, name: final n):
+        store.setAccent(c, n);
+        return;
+
+      case SetCardRadiusIntent(value: final v):
+        store.setCardRadius(v);
+        return;
+
+      case SetImageRadiusIntent(value: final v):
+        store.setImageRadius(v);
+        return;
+
+      case SetCustomBackgroundColorIntent(color: final c):
+        store.setCustomBackgroundColor(c);
+        return;
+
+      case SetCustomCardColorIntent(color: final c):
+        store.setCustomCardColor(c);
+        return;
     }
   }
 
