4775d9e7c11ad2c6e12a44ff92467875165fd0f7
Han <275905127@qq.com>
2026-01-17 20:07:28 +0800

    更新 design.dart

--- lib/design/design.dart
+++ lib/design/design.dart
@@ -1,7 +1,4 @@
 // lib/design/design.dart
-export 'tokens.dart';
-export 'theme.dart';
-export 'colors.dart';
-
-export '../widgets/foggy_app_bar.dart';
-export '../widgets/settings_widgets.dart';
+export 'app_colors.dart';
+export 'app_tokens.dart';
+export 'app_theme.dart';
